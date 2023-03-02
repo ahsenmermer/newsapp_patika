5d0427abcaf0401a0771042ea876c17e7188bba4
ahsenmermer <ahsenmermer@gmail.com>
2023-03-02 19:53:32 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { SafeAreaView, StyleSheet, Text, View, FlatList, ScrollView, Image, Dimensions } from 'react-native';
+import React from 'react';
+import NewsCard from './components/NewsCard';
+
+import news_data from './news_data.json';
+import news_banner_data from './news_banner_data.json';
+
+
+
+function App() {
+
+  const renderNews = ({ item }) => <NewsCard news={item} />;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.headertext}>News</Text>
+
+      <FlatList
+        ListHeaderComponent={() =>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {
+              news_banner_data.map(bannerNews => <Image style={styles.banner_image} source={{ uri: bannerNews.imageUrl }} />)
+            }
+          </ScrollView>}
+
+        keyExtractor={(item, index) => item.u_id.toString()}
+        data={news_data}
+        renderItem={renderNews}
+
+      />
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#eceff1j',
+  },
+  banner_image: {
+    height: Dimensions.get('window').height / 5,
+    width: Dimensions.get('window').width / 2,
+  },
+  headertext:{
+    fontSize:50,
+    fontWeight:'bold',
+  },
+
+})
+
+
+export default App;
